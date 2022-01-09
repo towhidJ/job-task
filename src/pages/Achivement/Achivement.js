0bce5eef2c5bb46422500982c3dab56effdae812
towhidJ <towhidulislamcse@gmail.com>
2022-01-09 11:23:42 +0600

    project-creacted

---
+++ src/pages/Achivement/Achivement.js
@@ -0,0 +1,12 @@
+import { Box, Container } from "@mui/material";
+import React from "react";
+
+const Achivement = () => {
+    return (
+        <Container maxWidth="sm">
+            <Box sx={{ bgcolor: "#cfe8fc", height: "100vh" }} />
+        </Container>
+    );
+};
+
+export default Achivement;
